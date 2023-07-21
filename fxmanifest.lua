49b6f4bea39f6ef90e047013db294b32418d150e
hoaaiww <75149340+hoaaiww@users.noreply.github.com>
2023-07-21 20:39:40 +0200

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,14 @@
+fx_version 'cerulean'
+game 'gta5'
+lua54 'yes'
+
+author 'hoaaiww'
+version '1.0'
+description 'A very simple and useful script to measure a vehicles 0-100 speed'
+
+client_script 'client.lua'
+server_script 'update_checker.lua'
+
+-- SETTINGS --
+use_kmh 'yes' -- if 'no' then the script will use MPH instead
+check_for_updates 'yes' -- if 'no' then the script wont check for updates
